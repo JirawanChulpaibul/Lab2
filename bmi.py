a7f864370a0ea5fa337096bb3c248ab0274dc8c5
JirawanChulpaibul <jirawan.21@ichat.sp.edu.sg>
2022-10-27 16:37:06 +0800

    bmi.py first change after push

--- bmi.py
+++ bmi.py
@@ -7,4 +7,11 @@ def calculate_bmi(height, weight) :
 
     print("Your bmi is " + str(format_bmi))
 
+    if bmi<18.5 :
+        print("Underweight")
+    elif bmi>=18.5 or bmi<=25.0 :
+        print("Normal Weight")
+    else :
+        print("Overweight")
+
 calculate_bmi(height=1.75,weight=57)
